b05aac397ce002c054852ee1922ba664d9726ae2
Ivan Vasilev <i.vasilev@dunice.net>
2022-11-22 14:16:58 +0300

    Add colors to build.sh script

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+clear='\033[0m'
+
+# Script
 if [ "dpkg -s cmake" ]; then
     [ -d build ] || mkdir build
     cmake . -Bbuild
     cmake --build ./build
 else
-    echo "Cmake is not installed"
+    echo -e "${red}Cmake is not installed.${clear}"
 fi
